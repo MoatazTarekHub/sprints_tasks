c5a1924d96923752a72e70a14857e4636e213224
MoatazTarekHub <86640380+MoatazTarekHub@users.noreply.github.com>
2021-09-05 09:17:55 +0200

    os

---
+++ RTOS/RTOS_Config.h
@@ -0,0 +1,16 @@
+/*
+ * RTOS_Config.h
+ *
+ *  Created on: Jan 10, 2021
+ *      Author: Al Badr
+ */
+
+#ifndef RTOS_CONFIG_H_
+#define RTOS_CONFIG_H_
+
+
+
+#define Task_Number  3
+
+
+#endif /* RTOS_CONFIG_H_ */
